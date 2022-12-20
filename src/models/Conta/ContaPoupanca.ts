90bfe795db1efe0cf1aeac295178b0e26910e443
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-12-20 18:00:56 -0300

    fix: NaN bug fixed in method calcularRendimento()

--- src/models/Conta/ContaPoupanca.ts
+++ src/models/Conta/ContaPoupanca.ts
@@ -1,39 +1,35 @@
-import { Conta } from "./abstract/Conta"
+import { Conta } from "./abstract/Conta";
 
 export class ContaPoupanca extends Conta {
-    constructor(
-        private rentabilidadeMensal: number,
-        numero: string
-        ) {
-            super(numero)
-        }
-        
-    public calcularRendimento(): number {
-        let saldoAtual: number;
-        
-        this.credito.forEach((credito) => {
-            saldoAtual = saldoAtual + credito.getValor()
-        })
+  constructor(private rentabilidadeMensal: number, numero: string) {
+    super(numero);
+  }
 
-        this.debito.forEach((debito) => {
-            saldoAtual = saldoAtual - debito.getValor()
-        })
+  public calcularRendimento(): number {
+    let saldoAtual = 0;
 
-        return saldoAtual * this.rentabilidadeMensal / 100
-    }
-        
-    public calcularSaldo(): number {
-        let saldoTotal = this.calcularRendimento()
+    this.credito.forEach((credito) => {
+      saldoAtual = saldoAtual + credito.getValor();
+    });
 
-        this.credito.forEach((credito) => {
-            saldoTotal = saldoTotal + credito.getValor()
-        })
+    this.debito.forEach((debito) => {
+      saldoAtual = saldoAtual - debito.getValor();
+    });
 
-        this.debito.forEach((debito) => {
-            saldoTotal = saldoTotal - debito.getValor()
-        })
+    return (saldoAtual * this.rentabilidadeMensal) / 10000;
+  }
 
-        return saldoTotal
+  public calcularSaldo(): number {
+    let saldoTotal = this.calcularRendimento();
 
-    }
+    this.credito.forEach((credito) => {
+      saldoTotal = saldoTotal + credito.getValor();
+    });
+
+    this.debito.forEach((debito) => {
+      saldoTotal = saldoTotal - debito.getValor();
+    });
+
+    return saldoTotal;
+  }
 }
