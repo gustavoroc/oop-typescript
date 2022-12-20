70d1ce95cb033095114c13ed87b4bbf353ae4043
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-12-20 14:55:13 -0300

    feat: the 'sacar' method of the class Conta, throw an exception if the user is forbiden to debit

--- src/models/Conta/abstract/Conta.ts
+++ src/models/Conta/abstract/Conta.ts
@@ -1,29 +1,26 @@
-import { Credito } from "../Credito"
-import { Debito } from "../Debito"
+import { ClientError } from "../../Error/ClientError";
+import { Credito } from "../Credito";
+import { Debito } from "../Debito";
 
 export abstract class Conta {
-    
-    public credito: Credito[] = []
-    public debito: Debito[] = []
+  public credito: Credito[] = [];
+  public debito: Debito[] = [];
 
-    constructor(
-        private numero: string,
-    ) {}
+  constructor(private numero: string) {}
 
-    public depositar(valor: number): void {
-        const credito = new Credito(valor, new Date(), this.numero)
-        this.credito.push(credito)
+  public depositar(valor: number): void {
+    const credito = new Credito(valor, new Date(), this.numero);
+    this.credito.push(credito);
+  }
+
+  public sacar(valor: number): void {
+    if (this.calcularSaldo() >= valor) {
+      const debito = new Debito(valor, new Date(), this.numero);
+      this.debito.push(debito);
+    } else {
+      throw new ClientError("Saldo insuficiente", 403);
     }
+  }
 
-    public sacar(valor: number): void {
-        if(this.calcularSaldo() >= valor) {
-            const debito = new Debito(valor, new Date(), this.numero)
-            this.debito.push(debito)
-        } else {
-            console.log("Não foi possivel efetuar a operação, seu saldo não é suficiente!")
-        }
-    }
-
-    abstract calcularSaldo(): Number
-
+  abstract calcularSaldo(): Number;
 }
